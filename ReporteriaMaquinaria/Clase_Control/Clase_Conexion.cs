07fde88151c20ea5e54a58dbdf5d89f726ba9d86
Rebeca Victoria Escobar Velásquez <rescobar@rosulgbm.dc>
2022-05-19 16:04:12 -0600

    primet commit

---
+++ ReporteriaMaquinaria/Clase_Control/Clase_Conexion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReporteriaMaquinaria.Clase_Control
+{
+    public class Clase_Conexion
+    {
+        #region "Declarations"
+        private string _servidor;
+        private string _base_de_datos;
+        private string _usuario;
+        private string _password;
+        #endregion
+        #region "Properties"
+        public string servidor
+        {
+            get { return _servidor; }
+            set { _servidor = value; }
+        }
+        public string base_de_datos
+        {
+            get { return _base_de_datos; }
+            set { _base_de_datos = value; }
+        }
+        public string usuario
+        {
+            get { return _usuario; }
+            set { _usuario = value; }
+        }
+        public string password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        #endregion
+
+        #region "Functions/Routines"
+        public void asignaDatosConeccionCrystal()
+        {
+
+            this.servidor = "10.102.1.237";
+            this.base_de_datos = "R2_Maquinaria";
+            this.usuario = "sap";
+            this.password = "12345678";
+            //this.password = "12345";
+        }
+        #endregion
+
+    }
+}
---
+++ ReporteriaMaquinaria/obj/Release/Package/PackageTmp/VisorControlDiario.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="VisorControlDiario.aspx.cs" Inherits="ReporteriaMaquinaria.VisorControlDiario" %>
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<%@ Register namespace="CrystalDecisions.Web" tagprefix="Web" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <p>
+&nbsp;&nbsp;&nbsp;
+        </p>
+        <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" ReportSourceID="CrystalReportSource1" />
+        <CR:CrystalReportSource ID="CrystalReportSource1" runat="server">
+            <report filename="Reportes\REPORTE_CONTROL_DIARIO.rpt">
+            </report>
+        </CR:CrystalReportSource>
+    </form>
+</body>
+</html>
